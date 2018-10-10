4dfdfd96760ca4fc67ab4ba009626aaee9067379
Rajesh <rajesh.das85@gmail.com>
2018-10-10 18:23:07 +0530

    Change Port

--- PrintAppServer/server.js
+++ PrintAppServer/server.js
@@ -73,7 +73,7 @@ app.use(errorHandler);
 
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 8080;
+const port = process.env.port === 'production' ? 80 : 4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
